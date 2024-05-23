d482a24c6c7575921eef9d121812665756b7c98e
Alexey Iliukevich <aleks.ilykevich@gmail.com>
2024-05-23 13:13:26 +0300

    refactor: use script setup

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,76 +2,71 @@
   <div class="input-container">
     <label>{{ label }}</label>
     <div class="input-wrapper">
-      <input 
-        v-model="innerValue"
-        v-bind="$attrs"
-      />
-      <button @click="clearInput">Очистить</button>
+      <input v-model="innerValue" v-bind="$attrs" />
+      <button @click="clearInput">Clear</button>
     </div>
-    <p v-if="countSearch > 0">Количество символов: {{ countSearch }}
+    <p v-if="countSearch > 0">
+      Количество символов: {{ countSearch }}
       <span v-if="isLimit" style="color: red;">Количество слов не должно превышать 3</span>
     </p>
   </div>
 </template>
 
-<script>
-import { ref, watch, computed } from 'vue';
+<script setup>
+import { ref, computed, defineProps, defineEmits, watch } from 'vue';
 
-export default {
-  props: {
-    label: String,
-    modelValue: String,
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
   },
-  emits: ['update:modelValue', 'clear'],
-  setup(props, { emit }) {
-    const wordsLimit = 3;
-    const isLimit = ref(false);
-    const innerValue = ref(props.modelValue);
-
-    const countSearch = computed(() => {
-      if (innerValue.value) {
-        return innerValue.value.length;
-      }
-      return 0;
-    });
-
-    watch(innerValue, (value) => {
-      checkWordsCount(value);
-    });
-
-    const clearInput = () => {
-      innerValue.value = '';
-      emit('clear');
-    };
-
-    const checkWordsCount = (value) => {
-      const wordsCount = value?.trim().split(/\s+/).length;
-      isLimit.value = wordsCount > wordsLimit;
-    };
-
-    return {
-      innerValue,
-      countSearch,
-      isLimit,
-      clearInput
-    };
+  label: {
+    type: String,
+    required: true
   }
+});
+
+const emit = defineEmits(['clear']);
+
+const wordsLimit = 3;
+const isLimit = ref(false);
+const innerValue = ref(props.modelValue);
+
+const checkWordsCount = (value) => {
+  const wordsCount = value?.trim().split(/\s+/).length;
+  isLimit.value = wordsCount > wordsLimit;
+};
+
+const countSearch = computed(() => {
+  if (innerValue.value) {
+    return innerValue.value.length;
+  }
+  return 0;
+});
+
+watch(innerValue, (value) => {
+  checkWordsCount(value);
+});
+
+const clearInput = () => {
+  innerValue.value = '';
+  emit('clear');
 };
 </script>
 
 <style scoped>
 .input-wrapper {
-    position: relative;
+  position: relative;
 }
 
 input {
-    padding: 5px;
+  padding: 5px;
 }
 
 button {
-    position: absolute;
-    left: 200px;
-    top: 3px;
-    padding: 5px;
+  position: absolute;
+  left: 200px;
+  top: 3px;
+  padding: 5px;
 }
 </style>
